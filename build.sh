fbb4dd5c6e03b0cf38bc69664806f4a2ce9a8d6c
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-02-11 19:08:37 +0300

    simple tcp server in c

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cc -O3 -o main main.c
+./main &
+
+pid=$!
+
+for i in {1..200}
+do
+  curl -Ss -X GET http://localhost:6969
+done
+
+kill $pid
---
+++ main.c
@@ -0,0 +1,51 @@
+#include <errno.h>
+#include <netdb.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <unistd.h>
+
+int main(void) {
+  // get addrinfo for the port 6969 to bind it to a socket
+  struct addrinfo hints = {.ai_family = AF_INET, .ai_socktype = SOCK_STREAM, .ai_flags = AI_PASSIVE};
+  struct addrinfo *addr = NULL;
+  getaddrinfo("127.0.0.1", "6969", &hints, &addr);
+
+  // create a new socket using the addrinfo we got
+  int socket_fd = socket(addr->ai_family, addr->ai_socktype, addr->ai_protocol);
+
+  // bind the new socket to the port 6969 using addrinfo we have
+  bind(socket_fd, addr->ai_addr, addr->ai_addrlen);
+
+  // free addrinfo since we do not need it anymore
+  freeaddrinfo(addr);
+
+  // listen on the socket (i don't know what this do tbh)
+  listen(socket_fd, 10);
+
+  // create the client sockaddr, this is somehow required by the accept function? even though we do not use it in here, but anyways
+  struct sockaddr client_addr;
+  socklen_t client_addrlen = sizeof(client_addr);
+
+  do {
+    // accept the new connection once it comes if it comes, this operation
+    // is blocking by nature, until a new connection is made then it lets the program keep running
+    int socket_client_fd = accept(socket_fd, &client_addr, &client_addrlen);
+
+    // here we are receiving the request that the client sent to us, and we are putting it in a buffer
+    char req[1024];
+    recv(socket_client_fd, req, 1024, 0);
+
+    // here we are sending the response to the client
+    const char *response = "HTTP/1.1 200 OK\r\nConnection: close\r\nContent-Type: text/plain\r\n\r\nHello World how are you guys doing today?\n";
+    send(socket_client_fd, response, strlen(response), 0);
+    close(socket_client_fd);
+  } while (true);
+
+  // close our socket
+  close(socket_fd);
+
+  return 0;
+}
