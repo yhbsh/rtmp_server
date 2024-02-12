625e8b3f171d8b84f2f1f4ab9afb45b3eb1a434e
HoussemBousmaha <ky_bousmaha@esi.dz>
2024-02-12 06:13:55 +0300

    improve build script

--- build.sh
+++ build.sh
@@ -2,15 +2,20 @@
 
 set -e
 
+HOST=127.0.0.1
+PORT=8080
+MAX_REQ=10
+
 rm -f log
 
 cc -O3 -o main main.c
-./main 10 &
 
-for i in {1..10}
+./main $MAX_REQ $PORT &
+
+for (( i = 0; i < $((MAX_REQ / 2)); i++))
 do
-  curl -Ss -X GET http://127.0.0.1:6969
+ curl -Ss -X GET  http://$HOST:8080
+ curl -Ss -X POST http://$HOST:8080 -H "Content-Type: text/plain; charset=utf-8" -d 'Hello World'
 done
-
 
 vi log
