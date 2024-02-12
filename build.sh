12dacee5086e285b724dad1cae0a79881e1af628
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-02-12 16:54:00 +0300

    modify build.sh to test rmtp

--- build.sh
+++ build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-set -e
+set -xe
 
 HOST=127.0.0.1
 PORT=8080
-URL=http://$HOST:$PORT
-MAX_REQ=10
+HTTP_URL=http://$HOST:$PORT
+RTMP_URL=rtmp://$HOST:$PORT
+MAX_REQ=1
 
 rm -f log
 
@@ -13,10 +14,6 @@ cc -O3 -o main main.c
 
 ./main $MAX_REQ $PORT &
 
-for (( i = 0; i < $((MAX_REQ / 2)); i++))
-do
- curl -Ss -X GET  $URL 
- curl -Ss -X POST $URL -H "Content-Type: text/plain; charset=utf-8" -d 'Hello World'
-done
+curl -Ss $RTMP_URL
 
 vi log
