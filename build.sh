819e90742354a27fc67633c9cf343d857bddf362
HoussemBousmaha <houssembousmaha.ksu@gmail.com>
2024-02-12 11:52:49 +0300

    create URL variable

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@ set -e
 
 HOST=127.0.0.1
 PORT=8080
+URL=http://$HOST:$PORT
 MAX_REQ=10
 
 rm -f log
@@ -14,8 +15,8 @@ cc -O3 -o main main.c
 
 for (( i = 0; i < $((MAX_REQ / 2)); i++))
 do
- curl -Ss -X GET  http://$HOST:8080
- curl -Ss -X POST http://$HOST:8080 -H "Content-Type: text/plain; charset=utf-8" -d 'Hello World'
+ curl -Ss -X GET  $URL 
+ curl -Ss -X POST $URL -H "Content-Type: text/plain; charset=utf-8" -d 'Hello World'
 done
 
 vi log
